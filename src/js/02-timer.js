4604e526529969214a3f6a77506966236275aafc
MarynaYa <yamarina30@gmail.com>
2022-09-06 15:41:12 +0300

    dz2-commit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -16,40 +16,76 @@ const refs = {
 
 // переменная обозначающая период от которого идет отсчет времени
 let selectedTime = null;
-updateClockface();
 
-// Таймер
-const timer = {
-    intervalId: null,
-    isActive: false,
-start() {
+// создаем прототип class Таймер
+class Timer {
+  constructor({onTick}) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.onTick = onTick;
+  }
+  start() {
     if(this.isActive) {
         return;
     }
-    const startTime = Date.now();
+    //const startTime = Date.now();
+    //refs.startBtn.textContent = `START`;
     this.isActive = true;
 
     this.intervalId = setInterval(() => {
         const currentTime = Date.now();
         const deltaTime = selectedTime - currentTime;
-        const time = convertMs(deltaTime);
-        updateClockface(time);
+        const time = this.convertMs(deltaTime);
+        this.onTick(time);
    // при 00.00.00.00 периода отсчета чистим таймер
         if (deltaTime <= 0) {
             this.isActive = false;
             clearInterval(this.intervalId);
+           // refs.startBtn.textContent = `Finish!`;
+            console.log( `Finish!`)
         }
     }, 1000);
 }
+// ф-ция для подсчета значений 
+convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = this.pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = this.pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = this.pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = this.pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
 }
+//console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+//console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+//console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+// ф-ция форматирует результат времени до двух чисел
+pad(value) {
+  return String(value).padStart(2, '0');
+}
+}
+
+// Таймер
+const timer = new Timer({
+  onTick: updateClockface, /// добавляем интерфейс в виде ф-ции
+});
+   
 // слушатель на кнопку Старт
 refs.startBtn.addEventListener(`click`, () => {
     timer.start();
 });
 
-
 // объект, который передается вторым аргументом для ф-ции flatpickr
-flatpickr(dataTimeInputEl, options);
 const options = {
     enableTime: true,
     time_24hr: true,
@@ -66,7 +102,9 @@ const options = {
     }
    }
     };
-  
+
+    flatpickr(refs.inputDate, options);
+
 // метод принимает день, час, мин., секунді и обновляет значения в интерфейсе
   function updateClockface({ days, hours, minutes, seconds}) {
     refs.days.textContent = days;
@@ -74,27 +112,3 @@ const options = {
     refs.minutes.textContent = minutes;
     refs.seconds.textContent = seconds;
   }
-
-  // ф-ция для подсчета значений 
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-  
-  //console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  //console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  //console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
